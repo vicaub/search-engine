f21c9d0bfa5e69dd01ac2f99f2fe6b1065408c50
Victor Aubin <aubin.victor@gmail.com>
2019-02-28 18:25:08 +0100

    added vectorial models

--- vectorial_model.py
+++ vectorial_model.py
@@ -54,12 +54,64 @@ def get_document_list_from_postig_lists(posting_lists):
 
 
 def w(t, d, posting_lists, dfts, N):
+    if t not in posting_lists:
+        return 0
     if d in posting_lists[t]:
         tftd = posting_lists[t][d]
     else:
         return 0
     dft = dfts[t]
-    return (1 + log(tftd, 10)) * log(N / dft, 10)
+    tf = (1 + log(tftd, 10))
+    idf = log(N / dft, 10)
+
+    return tf * idf
+
+
+# compute_normalization_coeff_memory = {}
+
+
+def compute_normalization_coeff(d, posting_lists, dfts, N, dict_term):
+    # if (d, str(posting_lists), str(dfts), N) in compute_normalization_coeff_memory:
+    #     return compute_normalization_coeff_memory[(d, str(posting_lists), str(dfts), N)]
+    # else:
+    s = 0
+    for term_id in dict_term.values():
+        s += w(term_id, d, posting_lists, dfts, N)
+        # compute_normalization_coeff_memory[(d, str(posting_lists), str(dfts), N)] = sqrt(s)
+    return sqrt(s)
+
+
+def w_normalized(t, d, posting_lists, dfts, N, dict_term):
+    if d in posting_lists[t]:
+        tftd = posting_lists[t][d]
+    else:
+        return 0
+    dft = dfts[t]
+    tf = (1 + log(tftd, 10))
+    idf = log(N / dft, 10)
+    norm = compute_normalization_coeff(d, posting_lists, dfts, N, dict_term)
+    if tf > 1:
+        return 1 + log(tf) * idf * (1 / norm)
+    else:
+        return 0
+
+
+def normalized_frequency(t, d, posting_lists, dfts, N, dict_term):
+    if d in posting_lists[t]:
+        tftd = posting_lists[t][d]
+    else:
+        return 0
+    dft = dfts[t]
+    # calcul des tf
+    tf = (1 + log(tftd, 10))
+    tf_maxi = 0
+    for term_id in dict_term.values():
+        if term_id in posting_lists:
+            tftd_tid = posting_lists[t][d]
+            tf_tid = (1 + log(tftd_tid, 10))
+            if tf_tid > tf_maxi:
+                tf_maxi = tf_tid
+    return tf / tf_maxi
 
 
 def vectorize_request(list_term_id, N, dfts):
@@ -69,10 +121,16 @@ def vectorize_request(list_term_id, N, dfts):
     return vector_request
 
 
-def vectorize_document(doc_id, list_term_id, N, dfts, posting_lists):
+def vectorize_document(doc_id, list_term_id, N, dfts, posting_lists, ponderation, dict_term):
     vector_doc = []
     for term in list_term_id:
-        vector_doc.append(w(term, doc_id, posting_lists, dfts, N))
+        if ponderation == "tfidf":
+            vector_doc.append(w(term, doc_id, posting_lists, dfts, N))
+        elif ponderation == "tfidf_norm":
+            vector_doc.append(w_normalized(term, doc_id, posting_lists, dfts, N, dict_term))
+        elif ponderation == "norm_freq":
+            vector_doc.append(normalized_frequency(term, doc_id, posting_lists, dfts, N, dict_term))
+
     return vector_doc
 
 
@@ -90,26 +148,31 @@ def find_nearest_cosinus_vector(vector_request, vector_doc_list):
             produit_scalaire += vector_request[i] * vector_doc[i]
             norme_1 += vector_request[i] ** 2
             norme_2 += vector_doc[i] ** 2
-        mesure = produit_scalaire / (sqrt(norme_1) * sqrt(norme_2))
+        if norme_2 == 0:
+            mesure = float('inf')
+        else:
+            mesure = produit_scalaire / (sqrt(norme_1) * sqrt(norme_2))
         if mesure <= minimum:
             minimum = mesure
             minimum_ind = doc_id
     return minimum_ind
 
 
-def modele_vectoriel(request, documents, dict_terms, type="CACM"):
+def modele_vectoriel(request, documents, dict_terms, ponderation="tfidf", type="CACM"):
+    compute_normalization_coeff_memory = {}
     docs = documents
     N = len(documents)
 
     dfts = get_dft_list("frequential_index/" + type.lower() + "/" + type.lower() + ".output")
 
     list_term_id = get_list_term_id_from_request(request, dict_terms)
-    posting_lists = get_posting_lists(list_term_id, "frequential_index/" + type.lower() + "/" + type.lower() + ".output")
+    posting_lists = get_posting_lists(list_term_id,
+                                      "frequential_index/" + type.lower() + "/" + type.lower() + ".output")
     docs = get_document_list_from_postig_lists(posting_lists)
 
     vector_doc_list = {}
     for doc in docs:
-        vector_doc_list[doc] = vectorize_document(doc, list_term_id, N, dfts, posting_lists)
+        vector_doc_list[doc] = vectorize_document(doc, list_term_id, N, dfts, posting_lists, ponderation, dict_terms)
 
     vector_request = vectorize_request(list_term_id, N, dfts)
 
